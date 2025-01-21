38581ab4e9b322d29ab079e47cf86c3a7a1b9e74
SACHIN SALEEM <89635387+s0ch1n@users.noreply.github.com>
2025-01-21 10:51:40 +0530

    Add files via upload

---
+++ Program22.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleAppAssignment3rd11K
+{
+    internal class Program22
+    {
+        //Bubble sort
+        static void Main(string[] args)
+        {
+            // Read the size of the array
+            Console.Write("Enter the size of the Array: ");
+            int size = Convert.ToInt16(Console.ReadLine());
+
+            int[] numList = new int[size];
+            int i;
+            Console.WriteLine("Enter the Array elements: ");
+            // Populate the array with user input
+            for (i = 0; i < size; i++)
+            {
+                numList[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            // Sorting logic using bubble sort
+            for (i = 0; i < size - 1; i++)
+            {
+                for (int j = 0; j < size - i - 1; j++)
+                {
+                    if (numList[j] > numList[j + 1])
+                    {
+                        // Swap the elements
+                        int temp = numList[j];
+                        numList[j] = numList[j+1];
+                        numList[j+1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted elements of the array using bubble sort: ");
+            for (i = 0; i < size; i++)
+            {
+                Console.Write(numList[i] + " ");
+            }
+        }
+    }
+}
---
+++ Program28.cs
@@ -0,0 +1,53 @@
+﻿namespace ConsoleAppAssignment4th6F
+{
+    internal class Program28
+    {
+        //To extract a substring from a given string.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To extract a substring from a given string");
+            //get input from the user
+            Console.Write("Enter the string: ");
+            string texts = Console.ReadLine();
+
+            // Getting starting position of the substring
+            Console.Write("Enter the starting position (0 - based index): ");
+            int startPosition;
+
+            //validating
+            while (!int.TryParse(Console.ReadLine(), out startPosition) || startPosition < 0 || startPosition >= texts.Length)
+            {
+                Console.WriteLine("Invalid input! Please enter a valid starting position within the string range.");
+            }
+
+            // Getting starting length of the substring
+
+            Console.Write("Enter the length of the substring: ");
+            int length;
+
+            //validating
+            while (!int.TryParse(Console.ReadLine(), out length) || length < 0 || startPosition + length > texts.Length)
+            {
+                Console.WriteLine("Invalid input! Please enter a valid length.");
+            }
+
+            // Extract substring
+            char[] substring = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                substring[i] = texts[startPosition + i];
+            }
+
+            // Displaying the result
+            Console.Write("Extracted Substring: ");
+            foreach (char sub in substring)
+            {
+                Console.Write(sub);
+            }
+
+            // To keep the console window open
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
